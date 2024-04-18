cc4f897442d405a4adf3fe2226b41f41abecd936
pmadhavan <madhavan.padmaja@gmail.com>
2024-04-18 09:51:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Tic Tac Toe game implementaion using Vanilla JS(TS)
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './style.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+ <h1>Sample Vanilla starter project</h1>
+`;
