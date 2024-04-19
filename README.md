26ab4eff099a2afc3d388e3f9d663d56a391b39c
pmadhavan <madhavan.padmaja@gmail.com>
2024-04-19 10:57:55 -0700

    Iteration 2 - Add gamer and winner logic

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 ## Tic Tac Toe game implementaion using Vanilla JS(TS)
+
+### Rules
+1. The game is played on a grid that's 3 squares by 3 squares.
+2. You are X , your friend (or the computer in this case) is O . 
+3. Players take turns putting their marks in empty squares.
+4. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.
+5. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.
+
+### Iteration 1- UI layout
+- In valilla JS how to i create the list of DOM nodes within a loop? Its not necessary to proceed becaus eI can hard code the 9 cells but good learning oppurtunity/
+    - We can use template string or function and append them.
+    - Decided to use creatElemnet method because it more efficient and flexible to control and manipulate the DOM for updates.
+- Was able to build a helper function to createElement that can be reused.
+
+__Comments__
+- Usage of Document Frgament
+- Using the helper function how to make the element accessible, like aria labels and tabIndex
+    - Add it when dynamic rendering within the loop
+- Adding event listener, since it is list use event propoagation and add listener to the parent div.
+- How to add click event and keypress event listenr
+    - Use array and loop through to add multiple listenrs
+
+### Iteration 2 - Updating DOM and game state based on user actions
+- Stuck with the state management concepts in JS
+- Managed to do the state with global variables and update the UI in click handler. Need review/
+
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,69 @@
 import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
- <h1>Tic Tac Toe</h1>
-`;
+const GRID = Array(9).fill(null);
+const WINNER =[
+    [0,1,2],[3,4,5],[6,7,8],[2,4,6],[0,4,8],[0,3,6],[1,4,7],[2,5,8]
+]
+const  createElementWithClass = (type: string, className?: string, text?:string) => {
+    const element = document.createElement(type);
+    element.className = className || '';
+    element.textContent = text || '';
+    return element;
+  }
+
+  const parentEle = createElementWithClass('div', 'parent');
+  const titleEle = createElementWithClass('h1', undefined,'Tic Tac Toe Game Board');
+  let currPlayer = 'X'
+  let gameState = GRID;
+  let isGameOver = false;
+  document.body.appendChild(titleEle);
+  document.body.appendChild(parentEle);
+
+  GRID.forEach((item, index) => {
+    const childCell = createElementWithClass('div', 'child');
+    childCell.tabIndex =0;
+    childCell.role='button'; 
+    childCell.id= index.toString();       
+    parentEle.appendChild(childCell);
+  });
+
+  ['click', 'keypress'].forEach((eventType) => {
+    parentEle.addEventListener(eventType, ( e:{target: HTMLDivElement})=>{
+        const target = e.target;
+        const cellValue= target.textContent;
+        const id = parseInt(target.id);
+        if(cellValue){
+            return;
+        }
+
+        target.textContent = currPlayer;
+        gameState[id] = currPlayer;
+        let nextPlayer = currPlayer === 'X' ? '0' : 'X';
+        currPlayer = nextPlayer;
+        isGameOver = checkFullBoard(gameState);
+        const winner= checkWinner();
+
+        const gameOverEle = createElementWithClass('h3','gameOver', 'Game Over!');
+        const winnerEle = createElementWithClass('h3','winner', `Palyer ${winner} Wins!!`);
+        winner? document.body.appendChild(winnerEle): isGameOver && document.body.appendChild(gameOverEle);
+
+        })
+  })
+  
+  const checkFullBoard = (gameState:string[])=>{
+    return  gameState.some((item) => !item) ? false: true;
+  }
+ 
+  const checkWinner = () => {
+    let winner ='';
+    WINNER.some((combo) => {
+        if(gameState[combo[0]] == gameState[combo[1]] && gameState[combo[1]] == gameState[combo[2]]){
+            winner = gameState[combo[0]];
+            return true;
+        }
+        return false;
+    });
+    return winner;
+  }
+
+
